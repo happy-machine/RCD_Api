6cd67b48428048a7af237d84aa8454f6aad727b2
happy_machine <thisisdjfresh@gmail.com>
2018-09-06 06:20:19 +0100

    trying cookieparser again

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ express().use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
-// express().use(cookieParser())
+
 
 
 
@@ -174,10 +174,12 @@ router.get('/invite', function (req, res) {
 });
 
 router.get('/callback', function (req, res) {
-  console.log('in host callback');
+  console.log('in host callback and req.cookies: ', req.cookies);
+  console.log('req.headers.cookies. ', req.headers.cookie.split(`${config.STATE_KEY}=`)[1])
   const code = req.query.code || null;
   const state = req.query.state || null;
-  const storedState = req.headers.cookie ? req.headers.cookie.split(`${config.STATE_KEY}=`)[1] : null;
+  const storedState = req.cookies ? req.cookies[config.STATE_KEY] : null;
+  // const storedState = req.headers.cookie ? req.headers.cookie.split(`${config.STATE_KEY}=`)[1] : null;
   console.log('STATE: ', state, 'STORED STATE: ', storedState)
   if (state === null || state !== storedState) {
     res.redirect('/#' +
@@ -218,7 +220,7 @@ router.get('/callback', function (req, res) {
 router.get('/guestcallback', function (req, res) {
   const code = req.query.code || null;
   const state = req.query.state || null;
-  const storedState = req.headers.cookie ? req.headers.cookie.split(`${config.STATE_KEY}=`)[1] : null;
+  const storedState = req.cookies ? req.cookies[config.STATE_KEY] : null;
   if (state === null || state !== storedState) {
     res.redirect('/#' +
       querystring.stringify({
@@ -327,6 +329,7 @@ const checkCurrentTrack = (user) => {
 // CONNECT TO WEBSOCKET THROUGH wss://<app-name>.herokuapp.com:443/socket
 const app = express()
 .use('/', router)
+.use(cookieParser())
 .listen(config.SERVER_PORT, () => console.log(`Listening on ${ config.SERVER_PORT }`));
 const wss = new SocketServer({ server: app , path: "/socket"});
 
