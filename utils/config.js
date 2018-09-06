03649b60bc1ec74f3901fa1f9bf84da4586d9e1b
happy_machine <thisisdjfresh@gmail.com>
2018-09-06 19:12:36 +0100

    babel heroku install

--- utils/config.js
+++ utils/config.js
@@ -12,9 +12,9 @@ const config = {
     STATE_KEY: 'spotify_auth_state',
     PLAYBACK_DELAY: 0,
     // Telegram
-    CHAT_ID: process.env.CHAT_ID,
-    TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN,
-    MAIN_ROOM: process.env.MAIN_ROOM,
+    CHAT_ID: process.env.CHAT_ID || null,
+    TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN || null,
+    MAIN_ROOM: process.env.MAIN_ROOM || null,
     // URL
     HOST_REDIRECT_URI: MODE === DEPLOY ? 'https://rcd-api.herokuapp.com/callback/' : `http://localhost:${SERVER_PORT}/callback/`,
     GUEST_REDIRECT_URI: MODE === DEPLOY ? 'https://rcd-api.herokuapp.com/guestcallback/' : `http://localhost:${SERVER_PORT}/guestcallback/`,
