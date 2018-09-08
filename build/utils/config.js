895574d455d41096eaa566dfdc9dc0d92e47d685
happy_machine <thisisdjfresh@gmail.com>
2018-09-08 20:02:57 +0100

    working on localhost

--- build/utils/config.js
+++ build/utils/config.js
@@ -10,7 +10,7 @@ var config = {
     CLIENT_SECRET: process.env.CLIENT_SECRET,
     SERVER_PORT: process.env.PORT || 5000,
     CLIENT_PORT: 3000,
-    MODE: _constants.LOCAL,
+    MODE: process.env.MODE,
     // Spotify
     PERMISSIONS_SCOPE: 'user-read-currently-playing user-modify-playback-state user-read-playback-state streaming user-read-private',
     STATE_KEY: 'spotify_auth_state',
