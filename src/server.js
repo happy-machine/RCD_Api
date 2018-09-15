64e90a3e03d3409fe1dddd5bba47fea6794b80ab
happy_machine <thisisdjfresh@gmail.com>
2018-09-15 15:17:44 +0100

    ..

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ express().use(allowCrossDomain);
 express().use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
