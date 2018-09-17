a107458f923791812d8c27fb4d1dd9d49ce3035b
Matthieu Nelmes <matthieunelmes@gmail.com>
2018-09-17 22:43:07 +0100

    send message functionality

--- src/server.js
+++ src/server.js
@@ -40,7 +40,7 @@ express().use(function (req, res, next) {
 });
 
 // Global state
-let wss
+let wss;
 let master = {
   track_uri: null,
   track_name: null,
@@ -52,55 +52,17 @@ let master = {
   album_cover: null,
 };
 const host = { token: null, name: null, id: null };
-let system_message_buffer = JSON.stringify({
-  type: '',
-  user_object: {},
-  master_object: {},
-  message: 'test',
-  roomId: null
-});
-let message_buffer = JSON.stringify({
-  type: '',
-  user_object: {},
-  master_object: {},
-  message: 'test',
-  roomId: null
-});
 
 // Instantiate rooms
 var roomService = new RoomService(new Array());
 
-
-const removeUser = (roomId, token, res) => {
-  console.log('trying to remove user in removeUser function')
-  let room_index = rooms.findIndex(x => x.roomId == roomId);
-  let user_index = rooms[room_index].users.findIndex(x => x.token == token);
-  if(room_index > -1 && user_index > -1){
-    rooms[room_index].users.splice(user_index, 1);
-    console.log('user removed');
-    //TODO: STOP PLAYBACK FOR REMOVED USER?
-    // RP(spotify.stopPlayback(token)).then((response)=>{
-    //   res.json(true);
-    // })
-    // .catch(e => console.log(e.message));
-  }
-  else if (rooms[room_index].host.token === token){
-     rooms[room_index] = {}
-    console.log('user is current host');
-    //TODO: REMOVE HOST AND ALL USERS?
-    res && res.json(true);
-  }else{
-    console.log('room or user could not be found');
-    res && res.json(false);
-  }
-}
 
 // Endpoints 
 // Host login
 router.get('/login', function (req, res) {
   const state = generateRandomString(16);
   res.cookie(config.STATE_KEY, state);
-  res.redirect(`https://accounts.spotify.com/authorize?${querystring.stringify(SpotifyService.spotifyOptions(urls.HOST_REDIRECT_URI[config.MODE], state))}`)
+  res.redirect(`https://accounts.spotify.com/authorize?${querystring.stringify(SpotifyService.spotifyOptions(urls.HOST_REDIRECT_URI[config.MODE], state))}`);
 });
 // Guest Login
 router.get('/invite', function (req, res) {
@@ -111,7 +73,7 @@ router.get('/invite', function (req, res) {
 });
 // Host Callback from spotify
 router.get('/callback', function (req, res) {
-  console.log('got host callback')
+  console.log('got host callback');
   const code = req.query.code || null;
   const state = req.query.state || null;
   const storedState = req.headers.cookie ? req.headers.cookie.split(`${config.STATE_KEY}=`)[1] : null;
@@ -131,19 +93,18 @@ router.get('/callback', function (req, res) {
             host.id = user_details.id;
             let roomId = generateRandomString(8);
             console.log('creating room for host: ', roomId);
-            roomService.createRoom({ roomId: roomId, host: host});
-            
-            system_message_buffer = makeBuffer(`${defaultNameCheck(host.name)} stepped up to the 1210s..`, host, {}, CONNECTION, roomId);
+            roomService.createRoom({ roomId: roomId, host: host });
             res.redirect(URLfactory('hostLoggedIn?' + querystring.stringify({ token: host.token, roomId: roomId, userName: host.name })));
+            sendMessage(makeBuffer(`${defaultNameCheck(host.name)} stepped up to the 1210s..`, host, {}, CONNECTION, roomId));
             pollUsersPlayback();
           })
           .catch(e => {
             res.redirect(URLfactory('getting_host_options', ERROR));
-            console.log('Getting host options ', e)
+            console.log('Getting host options ', e);
           });
       } else {
         res.redirect(URLfactory('spotify_host_auth', ERROR));
-        console.log('Spotify host auth ', e)
+        console.log('Spotify host auth ', e);
       }
     });
   }
@@ -156,8 +117,6 @@ router.get('/guestcallback', function (req, res) {
     res.redirect('/#' + queryStringError);
   } else {
     res.clearCookie(config.STATE_KEY);
-
-
     if (roomService.getRoom(roomId)) {
       let _room = roomService.getRoom(roomId);
       RP.post(SpotifyService.authOptions(urls.GUEST_REDIRECT_URI[config.MODE], code), function (error, response, body) {
@@ -178,23 +137,23 @@ router.get('/guestcallback', function (req, res) {
             .then(() => {
               // find room and add user
               roomService.addUserToRoom(roomId, newUser);
-              res.redirect(URLfactory('guestLoggedIn?' + querystring.stringify({ token: newUser.token, roomId: roomId, userName: newUser.name })))
-              wss.send(makeBuffer(`${defaultNameCheck(newUser.name)} joined the party...`, newUser, _room.master, CONNECTION, roomId));
+              sendMessage(makeBuffer(`${defaultNameCheck(newUser.name)} joined the party...`, newUser, _room.master, CONNECTION, roomId));
+              res.redirect(URLfactory('guestLoggedIn?' + querystring.stringify({ token: newUser.token, roomId: roomId, userName: newUser.name })));
+              
             })
             .catch(e => {
-              res.redirect(URLfactory('guest_sync', ERROR))
-              console.log('Error in guest sync ', e)
-            })
+              res.redirect(URLfactory('guest_sync', ERROR));
+              console.log('Error in guest sync ', e);
+            });
         } else {
-          res.redirect(URLfactory('guest_callback', ERROR))
-          console.log('Error in guest callback ', e)
+          res.redirect(URLfactory('guest_callback', ERROR));
+          console.log('Error in guest callback ', e);
         }
-      })
+      });
     } else {
       console.log('room does not exist');
       res.redirect('/#' + queryStringError);
     }
-
   }
 });
 
@@ -203,16 +162,14 @@ router.get('/removeuser', function (req, res) {
   console.log('attempting to remove user');
   const userId = req.query.id || null;
   const roomId = req.query.roomId || null;
-  const state = req.query.state || null;
   roomService.removeUser(roomId, userId);
 });
 
 // Get current track for room (I FIXED THIS)
 router.get('/getcurrenttrack', function (req, res) {
   const roomId = req.query.roomId;
-  let roomIndex = rooms.findIndex(x => x.roomId == roomId);
-  if (roomIndex > -1) {
-    res.json(rooms[roomIndex].master || null);
+  if (roomId) {
+    res.json(RoomService.getRoom(roomId).master || null);
   }
 });
 
@@ -231,14 +188,13 @@ router.get('/resetserver', function (req, res) {
 
 const syncToMaster = (host, users, roomId) => {
   if (host.id && users) {
-    console.log(host.id, users.length)
+    console.log(host.id, users.length);
     let _allRoomUsers = [...users, host];
     let _room = roomService.getRoom(roomId);
     let _master = {};
     // make reference to users, leave global users array immutable
     _allRoomUsers.some(
       (user) => {
-        // console.log('in sync at at user ', user.name, 'master track is ', _room.master.track_uri);
         wait_promise(350)
           .then(() => checkCurrentTrack(user))
           .then(result => {
@@ -250,10 +206,9 @@ const syncToMaster = (host, users, roomId) => {
                 .then((track) => {
                   _master.album_cover = track.album.images[0].url;
                   roomService.updateMaster(roomId, _master);
-                  
                   /* get the new tracks cover image and set the master to the new track that is taking over
                   then set the system message buffer to send update info to the client */
-                  system_message_buffer = makeBuffer(
+                  sendMessage(makeBuffer(
                     `${defaultNameCheck(_master.selector_name)} 
                       ${SELECTOR_CALLS[Math.floor(Math.random() * SELECTOR_CALLS.length)]} 
                       ${_master.track_name}!!`,
@@ -261,21 +216,17 @@ const syncToMaster = (host, users, roomId) => {
                     _master,
                     'track_change',
                     roomId
-                  );
-                  console.log('changing system buffer message')
-                  // wss.clients.forEach(function each(client) {
-                  //   client.send(system_message_buffer);
-                  // });
+                  ));
                   /* remove the current user from the reference to the array of users
                   and then run through all the remaining users setting their track details to master */
                   _allRoomUsers.splice(_allRoomUsers.indexOf(user), 1);
-                  console.log(' and now all users to send sync to are ', _allRoomUsers);
+                  // console.log(' and now all users to send sync to are ', _allRoomUsers);
                   resync(_allRoomUsers, _master);
                   return true;
                 });
             }
           })
-          .catch(e => console.log('Error in sync to master ', e.message))
+          .catch(e => console.log('Error in sync to master ', e.message));
       });
   } else {
     console.log('only one user in the room');
@@ -311,7 +262,7 @@ const checkCurrentTrack = (user) => {
         selector_name: user.name,
         selector_token: user.token,
         selector_id: user.id
-      }
+      };
       return resolve(master_ref);
     })
       .catch(e => reject(`in checkCurrentTrack ${e.message}`));
@@ -328,50 +279,44 @@ const app = express()
 wss = new SocketServer({ server: app, path: "/socket" });
 
 wss.on('connection', function connection(ws) {
-  console.log('ws connected')
-  ws.on('message', function (message) {
-    if (JSON.parse(message) !== '.') {
-      console.log('got message', JSON.parse(message))
-      console.log(wss.clients.entries().length, ' connections')
+  console.log('ws connected');
+  ws.on('message', function (messageData) {
+    if (JSON.parse(messageData) !== '.') {
+      console.log('got message', JSON.parse(messageData));
+      console.log(wss.clients.entries().length, ' connections');
     }
-    var message_rec = JSON.parse(message);
-    switch (message_rec.type) {
+    var message = JSON.parse(messageData);
+    switch (message.type) {
       case 'message':
-        message_buffer = JSON.stringify({
+        sendMessage(JSON.stringify({
           type: 'message',
-          userName: message_rec.userName || 'DJ Unknown',
+          userName: message.userName || 'DJ Unknown',
           master_object: master,
-          message: message_rec.message,
-          roomId: message_rec.roomId
-        }); break;
+          message: message.message,
+          roomId: message.roomId
+        })); break;
       case 'playback':
-        if (message_rec.message === "play_new") {
-          console.log('PLAY NEW TRACK!')
-          RP(SpotifyService.setPlaybackOptions(message_rec, message_rec, 0))
-          .catch(e => console.log(e.message))
-        } else if (message_rec.message === "pause") {
-          RP(SpotifyService.setPause(message_rec))
-          .catch(e => console.log(e.message))
-        } else if (message_rec.message === "play"){
-          RP(SpotifyService.setPlay(message_rec))
-          .catch(e => console.log(e.message))
-        }; break;
-      case 'close': roomService.removeUser(message_rec.roomId, message_rec.id); 
-      system_message_buffer = JSON.stringify({type: CONNECTION, message:`${message_rec.userName} left the room.`, roomId: message_rec.roomId});
-      break;
+        if (message.message === "play_new") {
+          console.log('PLAY NEW TRACK!');
+          RP(SpotifyService.setPlaybackOptions(message, message, 0))
+            .catch(e => console.log(e.message));
+        } else if (message.message === "pause") {
+          RP(SpotifyService.setPause(message))
+            .catch(e => console.log(e.message));
+        } else if (message.message === "play") {
+          RP(SpotifyService.setPlay(message))
+            .catch(e => console.log(e.message));
+        } break;
+      case 'close': roomService.removeUser(message.roomId, message.id);
+        sendMessage(JSON.stringify({ type: CONNECTION, message: `${message.userName} left the room.`, roomId: message.roomId }));
+        break;
       default: break;
     }
   });
-  // send system and message_buffer from global state every 200ms and then reset state
-  setInterval(
-    () => {
-
-      wss.clients.forEach((client) => {
-        message_buffer && client.send(message_buffer)
-        system_message_buffer && client.send(system_message_buffer)
-      });
-
-      message_buffer = ''
-      system_message_buffer = ''
-    }, 200)
 });
+
+const sendMessage = (message) => {
+  wss.clients.forEach((client) => {
+    client.send(message);
+  });
+};
